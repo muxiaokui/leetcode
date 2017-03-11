00e54b5160b139c0618cb655e28b84463741a8ad
muxiaokui <muxiaokui>
2017-03-11 23:06:51 +0800

    311 water

---
+++ 485 Max Consecutive Ones.cpp
@@ -0,0 +1,24 @@
+/*Author: JerryYan
+  data:0308
+  from:leetcode 485
+  comment: Ë®Ìâ
+*/
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ret = 0;
+        int now = 0;
+        int ll = nums.size();
+        for(int i=0;i<ll;i++){
+            if(nums[i] == 0){
+                ret = max(ret,now);
+                now = 0;
+            }
+            else{
+                now++;
+            }
+        }
+        return max(ret,now);
+    }
+};
