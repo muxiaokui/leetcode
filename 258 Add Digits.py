ee2394348b206d9e701f485ddf50c8a79cfd5508
muxiaokui <yjbntnc@gmail.com>
2017-03-16 21:20:43 +0800

    water py

---
+++ 258 Add Digits.py
@@ -0,0 +1,10 @@
+/*Author: JerryYan
+  comment:water py
+*/
+class Solution(object):
+    def addDigits(self, num):
+        if num == 0:
+            return 0
+        if num%9 == 0:
+            return 9
+        return num%9;
