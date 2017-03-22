781387e9ea5081dc1ae78aadb385a1d0dff36a08
muxiaokui <yjbntnc@gmail.com>
2017-03-22 21:25:48 +0800

    bitset && py的强大

---
+++ 401 Binary Watch.py
@@ -0,0 +1,9 @@
+/*Author: JerryYan
+   comment:py’Ê«ø¥Û°£°£°£
+*/
+class Solution(object):
+    def readBinaryWatch(self, num):
+        return ['%d:%02d' % (h,m)
+                for h in range(12) for m in range(60)
+                if(bin(h)+bin(m)).count('1') == num]
+
